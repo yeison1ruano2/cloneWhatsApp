209acd01cf4af27bce75fa43c3b3ab7ad2595a28
yeison1ruano2 <yeisonstiven100@gmail.com>
2025-01-18 15:49:37 -0500

    18/01/2025

---
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/WhatsAppCloneApiApplication.java
@@ -0,0 +1,13 @@
+package com.alibou.whatsappclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsAppCloneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsAppCloneApiApplication.class, args);
+	}
+
+}
---
+++ whatsappclone/src/test/java/com/alibou/whatsappclone/WhatsAppCloneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibou.whatsappclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsAppCloneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
